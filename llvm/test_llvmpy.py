6d60c4a46961a1ca9b8c64647cf4a8cbbdf83a52
Ilan Schnell <ilanschnell@gmail.com>
2012-09-12 15:18:12 -0500

    add inline test to common test suite

--- llvm/test_llvmpy.py
+++ llvm/test_llvmpy.py
@@ -16,7 +16,7 @@ else:
 
 from llvm import __version__
 from llvm.core import (Module, Type, GlobalVariable, Function, Builder,
-                       Constant, MetaData, MetaDataString)
+                       Constant, MetaData, MetaDataString, inline_function)
 import llvm.passes as lp
 import llvm.ee as le
 
@@ -404,6 +404,35 @@ class TestMetaData(unittest.TestCase):
         self.assertEqual(metadata.operands[1].string, 'abcdef')
 
 tests.append(TestMetaData)
+
+# ---------------------------------------------------------------------------
+
+class TestInlining(unittest.TestCase):
+    def test_inline_call(self):
+        mod = Module.new(__name__)
+        callee = mod.add_function(Type.function(Type.int(), [Type.int()]),
+                                  name='bar')
+
+        builder = Builder.new(callee.append_basic_block('entry'))
+        builder.ret(builder.add(callee.args[0], callee.args[0]))
+
+        caller = mod.add_function(Type.function(Type.int(), []),
+                                  name='foo')
+
+        builder = Builder.new(caller.append_basic_block('entry'))
+        callinst = builder.call(callee, [Constant.int(Type.int(), 1234)])
+        builder.ret(callinst)
+
+        pre_inlining = str(caller)
+        self.assertIn('call', pre_inlining)
+
+        self.assertTrue(inline_function(callinst))
+
+        post_inlining = str(caller)
+        self.assertNotIn('call', post_inlining)
+        self.assertIn('2468', post_inlining)
+
+tests.append(TestInlining)
 
 # ---------------------------------------------------------------------------
 
