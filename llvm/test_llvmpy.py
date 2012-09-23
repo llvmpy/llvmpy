81d6da3ae513cd56cd85c6d1f252c33e4082a299
Ilan Schnell <ilan@continuum.io>
2012-09-22 20:34:51 -0500

    add constant expression test

--- llvm/test_llvmpy.py
+++ llvm/test_llvmpy.py
@@ -185,6 +185,22 @@ tests.append(TestAtomic)
 
 # ---------------------------------------------------------------------------
 
+class TestConstExpr(unittest.TestCase):
+
+    def test_constexpr_opcode(self):
+        mod = Module.new('test_constexpr_opcode')
+        func = mod.add_function(Type.function(Type.void(), []), name="foo")
+        builder = Builder.new(func.append_basic_block('entry'))
+        a = builder.inttoptr(Constant.int(Type.int(), 123),
+                             Type.pointer(Type.int()))
+        self.assertTrue(isinstance(a, lc.ConstantExpr))
+        self.assertEqual(a.opcode, lc.OPCODE_INTTOPTR)
+        self.assertEqual(a.opcode_name, "inttoptr")
+
+tests.append(TestConstExpr)
+
+# ---------------------------------------------------------------------------
+
 class TestOperands(unittest.TestCase):
     # implement a test function
     test_module = """
