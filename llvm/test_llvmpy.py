7b0f3cc0c70612eac26b44ca2c949ea853489b3c
Ilan Schnell <ilan@continuum.io>
2012-09-22 20:59:03 -0500

    move engine builder test

--- llvm/test_llvmpy.py
+++ llvm/test_llvmpy.py
@@ -20,6 +20,7 @@ else:
 import llvm
 from llvm.core import (Module, Type, GlobalVariable, Function, Builder,
                        Constant, MetaData, MetaDataString, inline_function)
+from llvm.ee import EngineBuilder
 import llvm.core as lc
 import llvm.passes as lp
 import llvm.ee as le
@@ -380,6 +381,56 @@ entry:
         self.assertTrue(len(lp.PASSES)>0, msg="Cannot have no passes")
 
 tests.append(TestPasses)
+
+# ---------------------------------------------------------------------------
+
+class TestEngineBuilder(unittest.TestCase):
+
+    def make_test_module(self):
+        module = Module.new("testmodule")
+        fnty = Type.function(Type.int(), [])
+        function = module.add_function(fnty, 'foo')
+        bb_entry = function.append_basic_block('entry')
+        builder = Builder.new(bb_entry)
+        builder.ret(Constant.int(Type.int(), 0xcafe))
+        module.verify()
+        return module
+
+    def run_foo(self, ee, module):
+        function = module.get_function_named('foo')
+        retval = ee.run_function(function, [])
+        self.assertEqual(retval.as_int(), 0xcafe)
+
+
+    def test_enginebuilder_basic(self):
+        module = self.make_test_module()
+        ee = EngineBuilder.new(module).create()
+
+        with self.assertRaises(llvm.LLVMException):
+            # Ensure the module is owned.
+            llvm._util.check_is_unowned(module)
+
+        self.run_foo(ee, module)
+
+    def test_enginebuilder_force_jit(self):
+        module = self.make_test_module()
+        ee = EngineBuilder.new(module).force_jit().create()
+
+        self.run_foo(ee, module)
+
+    def test_enginebuilder_force_interpreter(self):
+        module = self.make_test_module()
+        ee = EngineBuilder.new(module).force_interpreter().create()
+
+        self.run_foo(ee, module)
+
+    def test_enginebuilder_opt(self):
+        module = self.make_test_module()
+        ee = EngineBuilder.new(module).opt(3).create()
+
+        self.run_foo(ee, module)
+
+tests.append(TestEngineBuilder)
 
 # ---------------------------------------------------------------------------
 
