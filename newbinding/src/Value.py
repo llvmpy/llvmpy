a3da74c416579ebdc6bc1f1b72dd68c7811b53e0
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-01-23 15:44:19 -0600

    Expand Value class

--- newbinding/src/Value.py
+++ newbinding/src/Value.py
@@ -2,7 +2,23 @@ from binding import *
 from namespace import llvm
 from raw_ostream import raw_ostream
 from AssemblyAnnotationWriter import AssemblyAnnotationWriter
-
+from Type import Type
+from LLVMContext import LLVMContext
+from StringRef import StringRef
+#
 Value = llvm.Class()
+
+dump = Value.method(Void)
+
 print_ = Value.method(Void, raw_ostream.Ref, AssemblyAnnotationWriter.Pointer)
 print_.realname = 'print'
+
+getType = Value.method(Type.Pointer)
+getContext = Value.method(LLVMContext.Ref)
+
+hasName = Value.method(Bool.To(bool))
+# skip getValueName, setValueName
+getName = Value.method(StringRef.To(str))
+setName = Value.method(Void, StringRef.From(str))
+
+replaceAllUsesWith = Value.method(Void, Value.Pointer)
