4b5cb100f71c5b9624a35e78c97324f48d0dde4f
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-01-28 15:18:24 -0600

    Auto discover binding modules

--- newbinding/src/__init__.py
+++ newbinding/src/__init__.py
@@ -1,2 +1,11 @@
-from . import Module
+import os.path, importlib
 
+def _init():
+    for fname in os.listdir(os.path.dirname(__file__)):
+        if ((fname.endswith('.py') or fname.endswith('.pyc')) and
+            not fname.startswith('__init__')):
+            modname = os.path.basename(fname).rsplit('.', 1)[0]
+            topname = __name__.rsplit('.', 1)[0]
+            importlib.import_module('.' + modname, topname)
+
+_init()
