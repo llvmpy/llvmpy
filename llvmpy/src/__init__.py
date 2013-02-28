e89c19ee60fed56ed43d243a69e540e3b9ff7f79
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-02-28 00:00:25 -0600

    Ensure order of imported modules

--- llvmpy/src/__init__.py
+++ llvmpy/src/__init__.py
@@ -2,7 +2,7 @@ import os.path
 
 def _init(root=__name__, file=__file__):
     base = os.path.dirname(file)
-    for fname in os.listdir(base):
+    for fname in sorted(os.listdir(base)):
         is_python_script = fname.endswith('.py') or fname.endswith('.pyc')
         is_init_script = fname.startswith('__init__')
         is_directory = os.path.isdir(os.path.join(base, fname))
