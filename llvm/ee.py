489d8056b944cd583a6f436d79535a7b75a1d548
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-05-03 10:57:38 -0500

    Fix error msg

--- llvm/ee.py
+++ llvm/ee.py
@@ -298,7 +298,7 @@ class TargetMachine(llvm.Wrapper):
         with contextlib.closing(BytesIO()) as error:
             target = api.llvm.TargetRegistry.lookupTarget(arch, triple, error)
             if not target:
-                raise llvm.LLVMException(error)
+                raise llvm.LLVMException(error.getvalue())
             if not target.hasTargetMachine():
                 raise llvm.LLVMException(target, "No target machine.")
             target_options = api.llvm.TargetOptions.new()
