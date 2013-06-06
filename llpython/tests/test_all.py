ee557c55cc37c12aef6844f99726a591dfcd13c2
Jon Riehl <jriehl@spaceship.com>
2013-06-06 14:13:39 -0500

    Added llpython.tests.test_all module for unit testing all llpython.

---
+++ llpython/tests/test_all.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# ______________________________________________________________________
+
+import unittest
+
+from .test_byte_control import TestByteControl
+from .test_byte_flow import TestBytecodeFlowBuilder
+from .test_addr_flow import TestAddressFlowBuilder
+
+# ______________________________________________________________________
+
+if __name__ == "__main__":
+    unittest.main()
+
+# ______________________________________________________________________
+# End of llpython/tests/test_all.py
