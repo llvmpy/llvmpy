b08a3467411f399b25365cc3b5c49d2a3cdd8bc6
anthony cantor <ar.cantor@gmail.com>
2013-09-02 23:55:51 -0600

    llvm.mc.Disassembler.decode now yields the byte sequence which generated the instruction
    added custom method to extra.h to return a PyBytes object
    from the bytes read from MemoryObject.readBytes.
    
    also moved the generic methods from StringRefMemoryObject
    up to MemoryObject

--- llvmpy/src/Support/StringRefMemoryObject.py
+++ llvmpy/src/Support/StringRefMemoryObject.py
@@ -8,7 +8,23 @@ if LLVM_VERSION >= (3, 4):
 
 @MemoryObject
 class MemoryObject:
-    pass
+    _include_ = "llvm/Support/MemoryObject.h"
+
+    getBase = Method(cast(Uint64, int))
+    getExtent = Method(cast(Uint64, int))
+
+    readBytes = CustomMethod('MemoryObject_readBytes',
+                             PyObjectPtr,
+                             cast(int, Uint64), #address
+                             cast(int, Uint64)  #size 
+                             )
+    @CustomPythonMethod
+    def readAll(self):
+        result = self.readBytes(self.getBase(), self.getExtent())
+        if not result:
+            raise Exception("expected readBytes to be successful!")
+        return result
+
 
 if LLVM_VERSION >= (3, 4):
     @StringRefMemoryObject
@@ -16,6 +32,3 @@ if LLVM_VERSION >= (3, 4):
         _include_ = "llvm/Support/StringRefMemoryObject.h"
 
         new = Constructor(cast(bytes, StringRef), cast(int, Uint64))
-
-        getBase = Method(cast(Uint64, int))
-        getExtent = Method(cast(Uint64, int))
