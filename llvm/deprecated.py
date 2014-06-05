8450c73cd14c5a09c96dc6c864d5cd72dea719c1
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-06-05 16:29:02 -0500

    Python3 fix

--- llvm/deprecated.py
+++ llvm/deprecated.py
@@ -18,8 +18,8 @@ def deprecated(func):
         warnings.warn_explicit(
             "Call to deprecated function %s." % (func.__name__,),
             category=DeprecationWarning,
-            filename=func.func_code.co_filename,
-            lineno=func.func_code.co_firstlineno + 1
+            filename=func.__code__.co_filename,
+            lineno=func.__code__.co_firstlineno + 1
         )
         return func(*args, **kwargs)
 
